0deb36f3e0122b2e18bc089efc74fd887952b9e7
Stefan-Comandant <stefancomandant@gmail.com>
2025-06-30 20:21:06 +0300

    removed keymapping from common.lua which loaded the wrong keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,13 +9,19 @@ map('i', '<C-j>', '<Down>', opts);
 map('i', '<C-k>', '<Up>', opts);
 map('i', '<C-l>', '<Right>', opts);
 
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts);
+--! NOTE: keymaps, somehow, only apply to .lua files
+-- integrate a function to split the current window and place the definition buffer on the bottom side
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { callback = function ()
+    print("hello there")
+    vim.api.nvim_command("split")
+end, noremap = true })
+
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts);
 map('n', 'gi', ':Telescope lsp_implementations<CR>', opts);
 map('n', 'gh', ':Telescope help_tags<CR>', opts);
 map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts);
 map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts);
-map('n', 'gl', '<cmd>Telescope diagnostics<CR>', opts);
+map('n', 'gl', '<cmd>Telescope diagnostics<CR>', { unique = true });
 map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts);
 map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts);
 
@@ -24,6 +30,8 @@ map('n', '<Tab>', ':BufferLineCycleNext<CR>', opts);
 map('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', opts);
 map('n', '<leader>tc', ':bdelete<CR>', opts) -- Close tab
 map('n', '<leader>/', ':nohlsearch<CR>', opts) -- Clear search highlights
+
+map('n', '<leader>bb', "<cmd>lua require\"stefan-plugin\".build()<CR>", opts)
 
 
 -- vim.keymap.set('n', '<leader>gi', function ()
@@ -34,9 +42,11 @@ map('n', '<leader>/', ':nohlsearch<CR>', opts) -- Clear search highlights
 map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
 map('n', '<leader>bf', ':Telescope buffers<CR>', opts)
 
-vim.api.nvim_create_user_command("Calculate", "lua require(\"calculator\").calculate()", { ["range"] = 1, ["nargs"] = 0 })
+vim.api.nvim_create_user_command("Calculate", "<cmd>lua require(\"calculator\").calculate()<CR>", { ["range"] = 1, ["nargs"] = 0 })
 
-vim.api.nvim_create_user_command("T", "Telescope", { ["range"] = 2, ["nargs"] = 1 })
+vim.api.nvim_create_user_command("T", "Telescope", { ["range"] = 3, ["nargs"] = 1 })
 
 map('v', '<leader>E', ':Calculate<CR>', opts);
 map('n', '<leader>E', ':Calculate<CR>', opts);
+
+print("This is the keymaps.lua!")
